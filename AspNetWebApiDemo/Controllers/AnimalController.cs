33d702a7529ac62d127ad6ab32521465da74615a
LL <nzlili2015@gmail.com>
2017-05-12 11:07:39 +1200

    Add web api demo to compare Mvc with WebApi

---
+++ AspNetWebApiDemo/Controllers/AnimalController.cs
@@ -0,0 +1,24 @@
+﻿using AspNetWebApiDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AspNetWebApiDemo.Controllers
+{
+    public class AnimalController : ApiController
+    { 
+        public IHttpActionResult GetAnimals()
+        {
+            var animals = new List<Animal>() {
+                new Animal() { Name = "Dog", Description = "A dog. " },
+                new Animal() { Name = "Cat", Description = "A cat. " },
+                new Animal() { Name = "Bird", Description = "A bird. " }
+            };
+
+            return Json(animals);
+        }
+    }
+}
---
+++ AspNetWebApiDemo/Models/Animal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AspNetWebApiDemo.Models
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Food { get; set; }
+    }
+}
