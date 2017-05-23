4e328ccdc9e8c64686db949a056330f43b15bbf8
LL <52593730@qq.com>
2017-05-23 15:08:20 +1200

    add comments

--- AspNetWebApiDemo/Global.asax.cs
+++ AspNetWebApiDemo/Global.asax.cs
@@ -7,8 +7,14 @@ using System.Web.Routing;
 
 namespace AspNetWebApiDemo
 {
-    public class WebApiApplication : System.Web.HttpApplication
+    public class WebApiApplication : HttpApplication
     {
+        /// <summary>
+        /// Called when the first resource (such as a page) in an ASP.NET application is requested. 
+        /// The Application_Start method is called only one time during the life cycle of an application. 
+        /// You can use this method to perform startup tasks such as loading data into the cache and initializing static values.
+        /// </summary>
+        /// <see cref="https://msdn.microsoft.com/en-us/library/ms178473(v=vs.100)"/>
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
